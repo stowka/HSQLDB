95ad67d4866fb8d336d146fc4e3d810b69f5d89a
stowka <antoine.degieter@me.com>
2014-03-31 15:25:54 +0100

    first commit

---
+++ src/uk/ac/port/hsqldb/SDBH.java
@@ -0,0 +1,33 @@
+package uk.ac.port.hsqldb;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ * 
+ * @author Antoine De Gieter
+ * @see org.hsqldb.*
+ *
+ */
+public final class SDBH {
+	public static Connection connection;
+	private SDBH() {
+		String url = "jdbc:hsqldb:file:";
+		String database = ".";
+		String driver = "org.hsqldb.jdbc.JDBCDriver";
+		String username = "SA";
+		String password = "";
+		try {
+			Class.forName(driver).newInstance();
+			SDBH.connection = (Connection)DriverManager.getConnection(url + database, username, password);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static synchronized Connection getConnection() {
+		if (SDBH.connection == null)
+			new SDBH();
+		return SDBH.connection;
+	}
+}
---
+++ src/uk/ac/port/hsqldb/Test.java
@@ -0,0 +1,36 @@
+package uk.ac.port.hsqldb;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Test {
+	public static void main(String[] argv) {
+		Connection conn = SDBH.getConnection();
+		try {
+			Statement stmt = conn.createStatement();
+			stmt.executeUpdate("DROP TABLE test IF EXISTS");
+			stmt.executeUpdate("DROP TABLE atom IF EXISTS");
+			stmt.executeUpdate(
+				"CREATE TABLE atom (" + 
+					"Z INT PRIMARY KEY COMMENT 'proton number'," +
+					"A INT COMMENT 'nucleon number'," +
+					"N INT COMMENT 'neutron number'," +
+					"symbol VARCHAR(2)" + 
+				")"
+			);
+			stmt.executeUpdate("INSERT INTO atom VALUES (1, 'H'), (12, 'C')");
+			ResultSet rset = stmt.executeQuery("SELECT * FROM atom");
+			while(rset.next()){
+				System.out.println(rset.getString("symbol"));
+			}
+			stmt.close();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+
+		}
+	}
+}

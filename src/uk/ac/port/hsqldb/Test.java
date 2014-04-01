f7d4578024f630bbad2a83385673a78003e061bc
stowka <antoine.degieter@me.com>
2014-04-01 09:21:50 +0100

    tests

--- src/uk/ac/port/hsqldb/Test.java
+++ src/uk/ac/port/hsqldb/Test.java
@@ -1,6 +1,7 @@
 package uk.ac.port.hsqldb;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -9,22 +10,15 @@ public class Test {
 	public static void main(String[] argv) {
 		Connection conn = SDBH.getConnection();
 		try {
+			PreparedStatement pstm = conn.prepareStatement("INSERT INTO t1 (col2, col3) VALUES (?, CURRENT_TIMESTAMP)");
+			pstm.setString(1, "Salut");
+			pstm.execute();
 			Statement stmt = conn.createStatement();
-			stmt.executeUpdate("DROP TABLE atom IF EXISTS");
-			stmt.executeUpdate(
-				"CREATE TABLE atom (" + 
-					"z INT PRIMARY KEY," + // proton number
-					"a INT," + // nucleon number
-					"n INT," + // neutron number
-					"symbol VARCHAR(2)" + 
-				")"
-			);
-			
-			stmt.executeUpdate("INSERT INTO atom VALUES (1, 1, 1, 'H'), (2, 4, 2, 'He'), (6, 12, 6, 'C')");
-			ResultSet rset = stmt.executeQuery("SELECT * FROM atom");
+			ResultSet rset = stmt.executeQuery("SELECT * FROM t1");
 			while(rset.next()){
-				System.out.println(rset.getInt("z") + "\t" + rset.getString("symbol"));
+				System.out.println(rset.getInt("col1") + "\t" + rset.getString("col2") + "\t" + rset.getString("col3"));
 			}
+			stmt.executeUpdate("SHUTDOWN"); // Saves the modifications into the file
 			stmt.close();
 			
 		} catch (SQLException e) {

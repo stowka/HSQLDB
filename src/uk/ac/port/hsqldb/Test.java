6ac8b3096e3537c60b3c2e5c7b6546ae144862de
Antoine De Gieter <antoine.degieter@me.com>
2014-04-02 14:21:44 +0100

    more relevant tables

--- src/uk/ac/port/hsqldb/Test.java
+++ src/uk/ac/port/hsqldb/Test.java
@@ -10,14 +10,56 @@ public class Test {
 	public static void main(String[] argv) {
 		Connection conn = SDBH.getConnection();
 		try {
-			PreparedStatement pstm = conn.prepareStatement("INSERT INTO t1 (col2, col3) VALUES (?, CURRENT_TIMESTAMP)");
-			pstm.setString(1, "Salut");
-			pstm.execute();
 			Statement stmt = conn.createStatement();
-			ResultSet rset = stmt.executeQuery("SELECT * FROM t1");
-			while(rset.next()){
-				System.out.println(rset.getInt("col1") + "\t" + rset.getString("col2") + "\t" + rset.getString("col3"));
-			}
+			
+			/*
+			 * 
+			 */
+			stmt.executeUpdate("DROP TABLE molecule IF EXISTS; DROP TABLE project IF EXISTS; DROP TRIGGER modify IF EXISTS");
+			
+			StringBuilder query = new StringBuilder();
+			
+			query.append("CREATE TABLE project (");
+			query.append("id INT IDENTITY,");
+			query.append("name VARCHAR(255),");
+			query.append("description VARCHAR(4095),");
+			query.append("creationTime TIMESTAMP DEFAULT NOW,");
+			query.append("modificationTime TIMESTAMP DEFAULT NOW");
+			query.append(");");
+			System.out.println(query.toString());
+			stmt.executeUpdate(query.toString());
+			
+			/*
+			 * Empties the query
+			 */			
+			query.delete(0, query.toString().length());
+			
+			query.append("CREATE TABLE molecule (");
+			query.append("id INT IDENTITY,");
+			query.append("name VARCHAR(255),");
+			query.append("smiles VARCHAR(1023),");
+			query.append("projectId INT,");
+			query.append("FOREIGN KEY (projectId) REFERENCES project(id)");
+			query.append(");");
+			System.out.println(query.toString());
+			stmt.executeUpdate(query.toString());
+			
+			query.delete(0, query.toString().length());
+			
+			query.append("CREATE TRIGGER modify AFTER UPDATE ON molecule ");
+			query.append("REFERENCING NEW AS newrow ");
+			query.append("FOR EACH ROW ");
+			query.append("BEGIN ATOMIC ");
+			query.append("UPDATE project SET modificationTime = NOW ");
+			query.append("WHERE id = newrow.projectId; ");
+			query.append("END;");
+			stmt.executeUpdate(query.toString());
+			
+			query.delete(0, query.toString().length());
+			
+			stmt.executeUpdate("INSERT INTO project (name, description) VALUES ('pro1', 'desc1'), ('pro2', 'desc2')");
+			stmt.executeUpdate("INSERT INTO molecule (name, smiles, projectId) VALUES ('mol1', 'cc', 1), ('mol2', 'ccc', 0), ('mol3', 'cc=cc', 1)");
+			
 			stmt.executeUpdate("SHUTDOWN"); // Saves the modifications into the file
 			stmt.close();
 			
